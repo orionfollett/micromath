64437721f852a1d58b08744beb2dd774f31e6629
Orion Follett <orionfollett@gmail.com>
2023-07-09 12:18:00 -0400

    vec classes memory layout done

--- micromath/src/micromath.h
+++ micromath/src/micromath.h
@@ -37,7 +37,7 @@ namespace micromath {
 			std::cout << "Vec2 " << vecArr[0] << " " << vecArr[1] << std::endl;
 		}
 	private:
-		float vecArr[2];
+		float vecArr[2] = {0, 0};
 	};
 
 	class Vector3 {
@@ -45,12 +45,33 @@ namespace micromath {
 		float& X() { return vecArr[0]; }
 		float& Y() { return vecArr[1]; }
 		float& Z() { return vecArr[2]; }
-
+		operator float* () { return (float*)this; }
 		void print() {
 			std::cout << "Vec3 " << vecArr[0] << " " << vecArr[1] << " " << vecArr[2] << std::endl;
 		}
 
 	private:
-		float vecArr[3];
+		float vecArr[3] = { 0, 0, 0 };
+	};
+
+	class Vector4 {
+	public:
+		float& X() { return vecArr[0]; }
+		float& Y() { return vecArr[1]; }
+		float& Z() { return vecArr[2]; }
+		float& W() { return vecArr[3]; }
+		operator float* () { return (float*)this; }
+		void print() {
+			std::cout << "Vec4 " << vecArr[0] << " " << vecArr[1] << " " << vecArr[2] << " " << vecArr[3] << std::endl;
+		}
+	private: 
+		float vecArr[4] = {0, 0, 0, 0};
+	};
+
+	//memory layout: 1D array to minimize memory allocation and deallocation
+	//override indexing operator to make it look like a 2D array
+	class Matrix2 {
+	public:
+
 	};
 }
